78265bd6e87f831fb35874af1b13e44b3f211f19
FlyingEwok <s254525@ocsb.local>
2020-09-24 12:32:04 -0400

    program

--- program5_1.py
+++ program5_1.py
@@ -1,6 +1,8 @@
 ''' Create a function that can accept two arguments name and age and print its value'''
 
+#Create function that prints a senetnce with variables that user sets when function is called
 def myFunction(name, age):
     print("Hello, your name is %s, and you're %s years old."%(name, age))
 
+#Call my function
 myFunction(input("Enter your name: "), input("Enter your age: "))
